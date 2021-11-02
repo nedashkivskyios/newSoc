571c106847339c990052ff728dd71c3845eb1544
nedashkivskyios <nedashkivskyios@gmail.com>
2021-11-02 16:56:22 +0200

    12

---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const Profile = () => {
+  return (
+    <div className={"content"}>
+      <div>
+        <img
+          src="https://lh3.googleusercontent.com/proxy/TSY_RD8o5SCRt4dU3HQXI5HptKOMkdoE32BvZkN_wOZcJBkmXd4lLqodcOvjr7V7wdmjbc5HSyxXa77n6-ZsLIUXmFfa-WTpaW4kTvHLe6N2"
+          alt="adsfasdf"/>
+      </div>
+      <div className={"avatar"}>
+        <img src="https://lwlies.com/wp-content/uploads/2017/04/avatar-2009.jpg" alt="avatar"/>
+        description
+      </div>
+      <div>
+        My posts
+      </div>
+    </div>
+  );
+};
